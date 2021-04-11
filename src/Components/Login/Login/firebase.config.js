7a20aa6e5a7737ce1165d2ad450b7087e2d57954
coderDaiyan <daiyanabdallah@gmail.com>
2021-04-11 21:57:02 +0600

    login ui done

--- src/Components/Login/Login/firebase.config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCdbvMTVUNq3yw50PqAfXh6Z0ml0qwHod8",
-  authDomain: "doctors-portal-df41c.firebaseapp.com",
-  projectId: "doctors-portal-df41c",
-  storageBucket: "doctors-portal-df41c.appspot.com",
-  messagingSenderId: "577549842884",
-  appId: "1:577549842884:web:ed05b4e741f9d1b1e54c91",
-};
